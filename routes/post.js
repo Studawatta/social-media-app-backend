6dbd081a67ce979b260774a8d76cb9b532b7d44d
Studawatta <st.udawatte@gmail.com>
2023-04-06 21:59:30 +0530

    unnecessary file deleted

--- routes/post.js
+++ routes/post.js
@@ -66,19 +66,31 @@ router.get('/:id', async (req, res) => {
 });
 //get timeline post
 
-router.get('/timeline/all', async (req, res) => {
+router.get('/timeline/:userId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
         return Post.find({ userId: friendId });
       })
     );
-    res.json(userPosts.concat(...friendPosts));
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (error) {
     res.status(500).json(error);
     console.log(error);
+  }
+});
+
+//get user's all posts
+
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    const posts = await Post.find({ userId: user._id });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
